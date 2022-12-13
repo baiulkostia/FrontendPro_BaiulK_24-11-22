93f681965a7e45538d208b5e99ea0c38dd3f22f3
Baiul Kostia <kostya.kadmin@gmail.com>
2022-12-13 15:12:36 +0200

    add hw5_iteration

---
+++ script.js
@@ -0,0 +1,51 @@
+// 1. Вивести на сторінку в один рядок через кому числа від 10 до 20.
+console.group('Вивести на сторінку в один рядок через кому числа від 10 до 20.');
+const number = 20;
+for (let i = 10; i <= number; i++){
+    console.log(i);
+}
+console.groupEnd();
+
+// 2. Вивести квадрати чисел від 10 до 20.
+console.group('Вивести квадрати чисел від 10 до 20.');
+for (let i = 10; i <= number; i++) {
+    console.log(i ** 2);
+}
+console.groupEnd();
+
+// 3. Вивести таблицю множення на 7.
+console.group('Вивести таблицю множення на 7.');
+for (let i = 0; i <= 10; i++) {
+    console.log(`${i} * ${7} = ${i*7}`);
+}
+console.groupEnd();
+
+// 4. Знайти добуток усіх цілих чисел від 15 до 35.
+console.group('Знайти добуток усіх цілих чисел від 15 до 35.');
+let mult = 1;
+for (let i = 15; i < 36; i++) {
+    mult = mult * i;
+    console.log(mult);
+}
+console.groupEnd();
+
+// 5. Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
+console.group('Знайти середнє арифметичне всіх цілих чисел від 1 до 500.');
+let sum = 0;
+for (let i = 1; i < 501; i++) {
+    sum = sum + i;
+    if (i === 500) {
+        console.log(sum / 500);
+    }
+}
+console.groupEnd();
+
+// 6. Вивести суму лише парних чисел в діапазоні від 30 до 80.
+console.group('Вивести суму лише парних чисел в діапазоні від 30 до 80.');
+let sum2 = 0
+for (let i = 30; i < 81; i++) {
+    if (i % 2 === 0) {
+        console.log(sum2 = sum2 + i);
+    }
+}
+console.groupEnd();
